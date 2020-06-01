dfaff58a75c2a087c21c840b905e59b59bdfb612
nikhilvkn <nikhilnarayanan2007@gmail.com>
2020-06-01 22:22:07 +0530

    adding comments for each route

--- server.py
+++ server.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 from inception import Server, Service, InceptionTools
 from pathlib import Path
-
 import pandas as pd
 import datetime
 import json
 
-
-
 app = Flask(__name__)
 
+# Home Page route
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-
+# Route that can route any strings
 @app.route('/<string:url_path>')
 def path(url_path):
     return render_template(url_path)
 
-
+# What will happen if I want to check what happens
+# when I submit the code. To know that.for testing
+# prupose, created this route
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
 	if request.method == 'POST':
@@ -27,6 +27,9 @@ def submit_form():
 		return json.dumps(data)
 
 
+# There is a service to check Inception microservices
+# which are running in less than 3 servers/instaces
+# in aws. This route logic helps perform that
 @app.route('/service_lessthan', methods=['POST', 'GET'])
 def service_lessthan():
 	if request.method == 'POST':
@@ -37,6 +40,13 @@ def service_lessthan():
 		return render_template('service_lessthan.html', data=full_data)
 
 
+# This route handles 3 different operations which I have
+# given to a user, 
+# 1) To list all servers in an inception datacenter [AWS]
+# 2) To list all servers specific to an environment in a dc
+# 3) To list servers based on the microservices given
+# There is option to add more than 1 microservices seperat-
+# ed by comma
 @app.route('/server-check', methods=['POST','GET'])
 def server_check():
    if request.method == 'POST':
@@ -64,6 +74,11 @@ def server_check():
          inception_request = Server(datacenter, environment)
          return render_template('service-check-result.html', data = inception_request.specific_server())
 
+
+# Defining the home directory and data time object
+# This route helps run the logic to create an excel
+# sheet with specific colums
+# Used Pandas module to manipulate the data frames
 home = str(Path.home())
 today = datetime.date.today()
 @app.route('/uploader', methods = ['GET', 'POST'])
@@ -77,7 +92,10 @@ def upload_cifile():
       dataframe.to_excel(f'{home}/Downloads/ci-approval{today}.xlsx', index = False)
       return render_template('render-out.html')
 
-      
+
+# Everyday, I need to gather and populate the change
+# request from service now. Used Pandas and modified
+# the dataframe to give the result as an excel sheet
 @app.route('/cruploader', methods = ['GET', 'POST'])
 def upload_crfile():
    if request.method == 'POST':
@@ -92,6 +110,10 @@ def upload_crfile():
       return render_template('render-out.html')
 
 
+# Based on the option added by the user, we can choose
+# 2 option. Either, we can list out all services in a 
+# datacenter in AWS, or can list out all servcers which
+# are specific to a particular environment like production
 @app.route('/service-check', methods = ['POST', 'GET'])
 def service():
    if request.method == 'POST':
